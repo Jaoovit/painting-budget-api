c8d43eb85ea3465b2d32703451f0e36a767a93c7
João Oliveira <jaoovit@gmail.com>
2024-12-24 15:37:04 +0000

    create a method to user login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,17 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async register(dto: RegisterDto) {
-    const password = dto.password;
-
     const saltOrRounds = 10;
 
-    const hash = await bcrypt.hash(password, saltOrRounds);
+    const hash = await bcrypt.hash(dto.password, saltOrRounds);
 
     try {
       const user = await this.prisma.user.create({
@@ -35,5 +34,23 @@ export class AuthService {
       }
       throw error;
     }
+  }
+
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('email incorrect');
+
+    const isMatch = await bcrypt.compare(dto.password, user.hash);
+
+    if (!isMatch) throw new ForbiddenException('password incorrect');
+
+    delete user.hash;
+
+    return user;
   }
 }
